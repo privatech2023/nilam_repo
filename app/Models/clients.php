215d06a0feda32d95e602c21598817c1cbd70bf0
LikanSoro <nilamdas000@gmail.com>
2024-01-10 15:23:45 +0530

    sync

--- app/Models/clients.php
+++ app/Models/clients.php
@@ -7,10 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class clients extends Authenticatable
 {
     use HasFactory, Notifiable;
+    use HasApiTokens;
     protected $table = 'clients';
 
     protected $primaryKey = 'client_id';
