20f4d5edbd9867d0e638345418286f46c254e7f2
privatech2023 <154963732+privatech2023@users.noreply.github.com>
2024-01-31 11:58:22 +0530

    ApiAuthController.php

--- app/Http/Controllers/Api/V1/ApiAuthController.php
+++ app/Http/Controllers/Api/V1/ApiAuthController.php
@@ -1,237 +1,170 @@
 <?php
 
-namespace App\Http\Controllers\Api\V1;
+namespace App\Http\Controllers\Api\v1;
 
 use App\Http\Controllers\Controller;
-use App\Http\Controllers\frontendController;
 use App\Models\clients;
-use App\Models\otp;
-use App\Models\subscriptions;
+use App\Models\messages;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 
-class ApiAuthController extends Controller
+class MessageSyncController extends Controller
 {
-    public function emailLogin(Request $request)
+    public function getLastMessage(Request $request)
     {
-        // Validate the request...
         $validator = Validator::make($request->all(), [
-            'email' => 'required|email|exists:clients,email',
-            'password' => 'required|string',
+            'device_id' => 'nullable|string',
+            'inbox' => 'required|boolean',
+            'device_token' => 'required'
         ]);
-        if ($validator->fails()) {
-            return response()->json([
-                'status' => false,
-                'message' => 'Login Failed',
-                'errors' => (object)$validator->errors()->toArray(),
-                'data' => (object)[],
-            ], 401);
-        }
-
-        $credentials = $request->only('email', 'password');
-
-        try {
-            if (!Auth::guard('client')->attempt($credentials)) {
-                return response()->json([
-                    'status' => false,
-                    'message' => 'Login Failed',
-                    'errors' => (object)[
-                        'email' => ['Incorrect email or password'],
-                    ],
-                    'data' => (object)[],
-                ], 401);
-            } else {
-                $user = clients::where('email', $credentials['email'])->first();
-
-                $token = $user->createToken('auth_token')->plainTextToken;
-                session()->put('auth_token', $token);
-                $activeSubscriptionEndDate = subscriptions::where('client_id', $user->client_id)
-                    ->where('status', 1)
-                    ->where('ends_on', '>=', date('Y-m-d'))
-                    ->orderByDesc('ends_on')
-                    ->value('ends_on');
-
-                // $user->update(['auth_token' => $token]);
-
-                $existingTokens = $user->auth_token ?: '';
-                $newTokenString = $existingTokens ? "$existingTokens,$token" : $token;
-                $user->update(['auth_token' => $newTokenString]);
-                return response()->json([
-                    'status' => true,
-                    'message' => 'Login Success',
-                    'errors' => (object) [],
-                    'data' => [
-                        'name' => $user->name,
-                        'email' => $user->email,
-                        'email_verified' => null,
-                        'mobile_number' => $user->mobile_number,
-                        'mobile_number_verified' => null,
-                        'has_active_subscription' => $activeSubscriptionEndDate ? true : false,
-                        'subscribed_upto' => $activeSubscriptionEndDate,
-                        'token' => $token,
-                    ],
-                ]);
-            }
-        } catch (\Exception $e) {
-            $errors = (object)[];
-            if (config('app.debug')) {
-                $errors = (object)[
-                    'exception' => [$e->getMessage()],
-                    'trace' => $e->getTrace(),
-                ];
-            }
-            return response()->json([
-                'status' => false,
-                'message' => 'Login Failed',
-                'errors' => $errors,
-                'data' => (object)[],
-            ], 500);
-        }
-    }
-
-    public function mobileOtp(Request $request)
-    {
-        // Validate the request...
-        $validator = Validator::make($request->all(), [
-            'mobile_number' => 'required|numeric|min:10',
-        ]);
-
 
         if ($validator->fails()) {
             return response()->json([
                 'status' => false,
-                'message' => 'OTP Failed',
+                'message' => 'Failed to retrieve last message',
                 'errors' => (object)$validator->errors()->toArray(),
+                'data' => (object)[],
+            ], 422);
+        }
+        $data = $request->only(['device_id', 'inbox', 'device_token']);
+
+        // $user = clients::where('client_id', session('user_id'))->first();
+        $token = str_replace('Bearer ', '', $request->header('Authorization'));
+        $user = clients::where('auth_token', $token)->where('device_id', $data['device_id'])->where('device_token', $data['device_token'])->first();
+        $device_id = $data['device_id'] ?? $user->device_id;
+        if ($user == null) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Authorization failed',
+                'errors' => (object)[],
+                'data' => (object)[],
+            ], 406);
+        }
+        if (!$device_id) {
+            return response()->json([
+                'status' => false,
+                'message' => 'No device ID found',
+                'errors' => (object)[],
+                'data' => (object)[],
+            ], 406);
+        }
+
+        $last_message = DB::table('messages')
+            ->where('device_id', $device_id)
+            ->where('is_inbox', $data['inbox'])
+            ->orderBy('message_id', 'desc')
+            ->first();
+
+        if (!$last_message) {
+            return response()->json([
+                'status' => false,
+                'message' => 'No messages found',
+                'errors' => (object)[],
                 'data' => (object)[],
             ], 404);
         }
-        $credentials = $request->only('mobile_number');
 
-        try {
-            // Check if user with mobile number exists
-            $user = clients::where('mobile_number', $credentials['mobile_number'])->first();
-
-            if (!$user) {
-                return response()->json([
-                    'status' => false,
-                    'message' => 'User not found',
-                    'errors' => (object)[
-                        'mobile_number' => ['User not found'],
-                    ],
-                    'data' => (object)[],
-                ], 404);
-            }
-            $model = new otp();
-            // Generate OTP
-            $otp = rand(100000, 999999);
-            $message = $message = $otp . ' is the OTP to login at RTS. Valid for 1 min only. RTS LLP';
-
-            $data = [
-                'otp' => $otp,
-                'isexpired' => 1,
-                'mobile' => $credentials['mobile_number'],
-            ];
-            $model->create($data);
-
-            $frontendcontroller = new frontendController;
-            $frontendcontroller->sendOTP($credentials['mobile_number'], $message);
-            return response()->json([
-                'status' => true,
-                'message' => 'OTP Sent',
-                'errors' => (object)[],
-                'data' => (object)[],
-            ]);
-        } catch (\Exception $e) {
-            $errors = (object)[];
-            if (config('app.debug')) {
-                $errors = (object)[
-                    'exception' => [$e->getMessage()],
-                    'trace' => $e->getTrace(),
-                ];
-            }
-            return response()->json([
-                'status' => false,
-                'message' => 'OTP Failed',
-                'errors' => $errors,
-                'data' => (object)[],
-            ], 500);
-        }
+        return response()->json([
+            'status' => true,
+            'message' => 'Last message retrieved',
+            'errors' => (object)[],
+            'data' => (object)[
+                'last_message' => (object)[
+                    'message_id' => $last_message->message_id,
+                    'device_id' => $last_message->device_id,
+                    'number' => $last_message->number,
+                    'date' => $last_message->date,
+                    'body' => $last_message->body,
+                    'is_inbox' => $last_message->is_inbox,
+                ]
+            ],
+        ], 200);
     }
 
 
-    public function mobileOtpVerify(Request $request)
+    public function uploadMessages(Request $request)
     {
-        // Validate the request...
+        // return response()->json([$request->header('Authorization')]);
         $validator = Validator::make($request->all(), [
-            'mobile_number' => 'required|numeric|exists:clients,mobile_number',
-            'otp' => 'required|string',
+            'device_id' => 'nullable|string',
+            'device_token' => 'required',
+            'inbox' => 'required|boolean',
+            'json_file' => 'required|file|mimes:json,txt|max:10000',
         ]);
 
         if ($validator->fails()) {
             return response()->json([
                 'status' => false,
-                'message' => 'OTP verification failed',
+                'message' => 'Failed to upload messages',
                 'errors' => (object)$validator->errors()->toArray(),
                 'data' => (object)[],
-            ], 401);
+            ], 422);
         }
 
-        $credentials = $request->only('mobile_number', 'otp');
+        $data = $request->only(['device_id', 'inbox', 'json_file', 'device_token']);
+        $token = str_replace('Bearer ', '', $request->header('Authorization'));
+        $user = clients::where('auth_token', $token)->where('device_id', $data['device_id'])->where('device_token', $data['device_token'])->first();
+        if ($user == null) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Authorization failed',
+                'errors' => (object)[],
+                'data' => (object)[],
+            ]);
+        }
+        $device_id = $data['device_id'] ?? $user->device_id;
+
+        if (!$device_id) {
+            return response()->json([
+                'status' => false,
+                'message' => 'No device found',
+                'errors' => (object)[],
+                'data' => (object)[],
+            ], 406);
+        }
+        $json_file = $data['json_file'];
+        // Store the file in storage/app/messages/inbox or storage/app/messages/outbox
+        $json_file_path = 'messages/' . ($data['inbox'] ? 'inbox' : 'outbox') . '/' . $json_file->getClientOriginalName();
+        $json_file->storeAs('messages/' . ($data['inbox'] ? 'inbox' : 'outbox'), $json_file->getClientOriginalName());
+
+        // Read the file contents
+        $json_file_content = file_get_contents(storage_path('app/' . $json_file_path));
+        $json_file_content = json_decode($json_file_content, true);
 
         try {
-            // Check if user with mobile number exists
-            $user = clients::where('mobile_number', $credentials['mobile_number'])->first();
+            $messages = $json_file_content['data'];
 
-            if (!$user) {
-                return response()->json([
-                    'status' => false,
-                    'message' => 'User not found',
-                    'errors' => (object)[
-                        'mobile_number' => ['User not found'],
-                    ],
-                    'data' => (object)[],
-                ], 404);
+            $messagesToInsert = [];
+            $now = now();
+
+            foreach ($messages as $message) {
+                if ((messages::where('message_id', $message['message_id'])->first()) != null) {
+                    continue;
+                }
+                $message['user_id'] = $user->client_id;
+                $message['device_id'] = $device_id;
+                $message['message_id'] = $message['message_id'];
+                $message['number'] = $message['number'];
+                $message['date'] = $message['date'];
+                $message['body'] = $message['body'];
+                $message['is_inbox'] = $data['inbox'];
+                $message['created_at'] = $now;
+                $message['updated_at'] = $now;
+                $messagesToInsert[] = $message;
             }
 
-            // Verify OTP
-            $user_otp = otp::where('mobile', $credentials['mobile_number'])
-                ->orderBy('created_at', 'desc')
-                ->first();
-
-            if ($user_otp->otp != $credentials['otp']) {
-                return response()->json([
-                    'status' => false,
-                    'message' => 'OTP verification failed',
-                    'errors' => (object)[
-                        'otp' => ['OTP verification failed'],
-                    ],
-                    'data' => (object)[],
-                ], 401);
+            foreach (array_chunk($messagesToInsert, 1000) as $chunk) {
+                DB::table('messages')->insert($chunk);
             }
 
-            $token = $user->createToken('auth_token')->plainTextToken;
-
-            $existingTokens = $user->auth_token ?: '';
-            $newTokenString = $existingTokens ? "$existingTokens,$token" : $token;
-            $user->update(['auth_token' => $newTokenString]);
-            $activeSubscriptionEndDate = subscriptions::where('client_id', $user->client_id);
-            return response()->json([
-                'status' => true,
-                'message' => 'Login Success',
-                'errors' => (object) [],
-                'data' => [
-                    'name' => $user->name,
-                    'email' => $user->email,
-                    'email_verified' => null,
-                    'mobile_number' => $user->mobile_number,
-                    'mobile_number_verified' => null,
-                    'has_active_subscription' => $activeSubscriptionEndDate ? true : false,
-                    'subscribed_upto' => $activeSubscriptionEndDate,
-                    'token' => $token,
-                ],
-            ]);
+            // Delete the file
+            unlink(storage_path('app/' . $json_file_path));
         } catch (\Exception $e) {
+            // Delete the file
+            unlink(storage_path('app/' . $json_file_path));
+
             $errors = (object)[];
             if (config('app.debug')) {
                 $errors = (object)[
@@ -239,34 +172,20 @@ class ApiAuthController extends Controller
                     'trace' => $e->getTrace(),
                 ];
             }
+
             return response()->json([
                 'status' => false,
-                'message' => 'Login Failed',
+                'message' => 'Failed to upload messages',
                 'errors' => $errors,
                 'data' => (object)[],
             ], 500);
         }
-    }
 
-    public function logout(Request $request)
-    {
-        // $validator = Validator::make($request->all(), [
-        //     'device_token' => 'required|numeric|min:10',
-        // ]);
-
-
-        // if ($validator->fails()) {
-        //     return response()->json([
-        //         'status' => false,
-        //         'message' => 'Validation error',
-        //         'errors' => (object)$validator->errors()->toArray(),
-        //         'data' => (object)[],
-        //     ], 404);
-        // }
-
-        // $user = clients::where('mobile_number')
         return response()->json([
-            session('auth_token')
-        ]);
+            'status' => true,
+            'message' => 'Messages uploaded',
+            'errors' => (object)[],
+            'data' => (object)[],
+        ], 200);
     }
 }
