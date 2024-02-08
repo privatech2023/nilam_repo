4cd46f23fe3d2dbe0f13349528235e34d2a1a076
LikanSoro <nilamdas000@gmail.com>
2024-02-08 12:12:30 +0530

    skajh

--- resources/views/livewire/video-record-component.blade.php
+++ resources/views/livewire/video-record-component.blade.php
@@ -1,15 +1,17 @@
 <div>
-@if(session('user_name'))
-<div class="row mt-3 welcome">
-    <div class="col-9 ">
-        <h2 class="welcome-text">Welcome, {{session('user_name')}}</h2>
+    <div class="bread" >
+        <div >
+            <a href="{{url('/')}}" style="text-decoration: none;">
+                <button type="button" class="btn btn-md" style="margin-top: 7px; background-color: #60377b; border-radius: 30px; color: white; box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-arrow-left-short" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5"/>
+                    </svg>
+                </button>
+            </a>
+        </div>
+        
+            @livewire('dropdown')
     </div>
-    <div>
-        @livewire('dropdown')
-    </div>
-</div>
-
-@endif
 
 <div class="content-wrapper remove-background">
     <div id="frame">
