f868f36d868ca630477c3a5068f44d52e716fa43
LikanSoro <tek2991@gmail.com>
2024-02-25 15:30:43 +0530

    ss

--- app/Http/Livewire/AlertDeviceComponent.php
+++ app/Http/Livewire/AlertDeviceComponent.php
@@ -17,7 +17,7 @@ class AlertDeviceComponent extends Component
 
         $client_id = clients::where('client_id', session('user_id'))->first();
         $client = device::where('device_id', $client_id->device_id)->where('client_id', $client_id->client_id)->orderBy('updated_at', 'desc')->first();
-        if ($client->device_token == null) {
+        if ($client == null) {
             $this->dispatchBrowserEvent('banner-message', [
                 'style' => 'danger',
                 'message' => 'No Device token! Please register your device first',
