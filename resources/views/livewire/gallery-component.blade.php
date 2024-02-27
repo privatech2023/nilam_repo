8868001bd6828e2fd2fd3a72f9eb698e37dca38d
privatech2023 <154963732+privatech2023@users.noreply.github.com>
2024-02-27 15:56:45 +0530

    Update gallery-component.blade.php

--- resources/views/livewire/gallery-component.blade.php
+++ resources/views/livewire/gallery-component.blade.php
@@ -114,7 +114,7 @@
         </style>
         <div class="image-container" style="display: flex; flex-wrap: wrap; justify-content: space-between; height: auto; overflow-y: auto;">
 
-            {{-- <div class="row">
+             <div class="row">
                 @foreach($gallery_items as $image)
                 <div class="image-wrapper">
                 <img src="{{ $image->s3Url() }}" alt="{{$image->id}}" style=" object-fit: cover; margin-right: 10px; border-radius: 6px;">
@@ -129,7 +129,7 @@
                     @if($gallery_items->count() % 4 == 0) 
                     <button class="btn btn-link p-0 m-0 text-primary" wire:click="loadMore">Load More</button>
                     @endif
-                </div> --}}
+                </div> 
 
             
             
