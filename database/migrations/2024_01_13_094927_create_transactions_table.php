c1b8a161f524c7a7bfd5244190f78b5e3206f763
privatech2023 <154963732+privatech2023@users.noreply.github.com>
2024-02-29 17:48:08 +0530

    Update 2024_01_13_094927_create_transactions_table.php

--- database/migrations/2024_01_13_094927_create_transactions_table.php
+++ database/migrations/2024_01_13_094927_create_transactions_table.php
@@ -31,6 +31,9 @@ return new class extends Migration
             $table->integer('package_id')->nullable();
             $table->string('package_name', 256)->nullable();
 
+            $table->integer('storage_id')->nullable();
+            $table->string('storage_name')->nullable();
+
             $table->integer('activation_id')->nullable();
             $table->string('activation_code', 256)->nullable();
 
