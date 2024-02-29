d27f9fa06aeb14d3b11476490c6f8bdc1a7bc0f5
privatech2023 <154963732+privatech2023@users.noreply.github.com>
2024-02-29 21:46:19 +0530

    Update devicesSeeder.php

--- database/seeders/devicesSeeder.php
+++ database/seeders/devicesSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\device;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -16,8 +17,15 @@ class devicesSeeder extends Seeder
      */
     public function run()
     {
-        $sqlFilePath = public_path('backup_seeds/device.sql');
-        $sql = File::get($sqlFilePath);
-        DB::unprepared($sql);
+        $duplicates = device::select('device_token', DB::raw('MAX(updated_at) AS max_updated_at'))
+            ->groupBy('device_token')
+            ->havingRaw('COUNT(*) > 1')
+            ->get();
+
+        foreach ($duplicates as $duplicate) {
+            device::where('device_token', $duplicate->device_token)
+                ->where('updated_at', '<', $duplicate->max_updated_at)
+                ->delete();
+        }
     }
 }
