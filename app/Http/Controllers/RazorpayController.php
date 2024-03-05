4bade6b71ad844b83218180c937431eb1e5e2208
privatech2023 <154963732+privatech2023@users.noreply.github.com>
2024-03-05 11:34:12 +0530

    Update RazorpayController.php

--- app/Http/Controllers/RazorpayController.php
+++ app/Http/Controllers/RazorpayController.php
@@ -57,7 +57,7 @@ class RazorpayController extends Controller
                     $subscription = subscriptions::where('txn_id', $transaction->txn_id)->first();
                     if ($subscription != null) {
                         $subscription->update([
-                            'status' => 2
+                            'status' => 1
                         ]);
                     }
                 }
@@ -102,7 +102,7 @@ class RazorpayController extends Controller
                     } else {
                         $subscription = subscriptions::where('txn_id', $payment->txn_id)->first();
                         $subscription->update([
-                            'status' => 2
+                            'status' => 1
                         ]);
                     }
                     Log::error('webhook 3');
