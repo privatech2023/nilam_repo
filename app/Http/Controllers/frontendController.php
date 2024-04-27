7d0ce430287fd2ccf292afb3144fcf75a627a958
privatech2023 <154963732+privatech2023@users.noreply.github.com>
2024-04-27 10:33:59 +0530

    Update frontendController.php

--- app/Http/Controllers/frontendController.php
+++ app/Http/Controllers/frontendController.php
@@ -50,15 +50,15 @@ class frontendController extends Controller
             if ($bg == null) {
                 $isGall = 0;
             }
-            if ($bg->image_id != 0 && $bg->image_id != null) {
-                $isGall = 2;
-                $image = gallery_items::where('id', $bg->image_id)->first();
-                if ($image == null) {
-                    $image = [];
-                }
-            } else {
-                $image = [];
-            }
+            // if ($bg->image_id != 0 && $bg->image_id != null) {
+            //     $isGall = 2;
+            //     $image = gallery_items::where('id', $bg->image_id)->first();
+            //     if ($image == null) {
+            //         $image = [];
+            //     }
+            // } else {
+            //     $image = [];
+            // }
         } else {
             $bg = 0;
             $image = [];
