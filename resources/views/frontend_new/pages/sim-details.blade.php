964685a5bbdda31ce929aeadea55a9c1551570e1
privatech2023 <154963732+privatech2023@users.noreply.github.com>
2024-05-02 14:58:37 +0530

    Update sim-details.blade.php

--- resources/views/frontend_new/pages/sim-details.blade.php
+++ resources/views/frontend_new/pages/sim-details.blade.php
@@ -1,174 +1,101 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sim Details</title>
+namespace App\Http\Controllers\Api\V1;
 
-    <!-- Favicons -->
-    <link href="{{ asset('assets/frontend/images/favicon-32x32.png')}}" rel="icon">
-    <link href="{{ asset('assets_2/img/apple-touch-icon.png" rel="apple-touch-icon')}}" rel="apple-touch-icon">
+use App\Http\Controllers\Controller;
+use App\Models\clients;
+use App\Models\device;
+use App\Models\sim_details;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
 
-    <!-- bootstrap CDN -->
-
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
-    <!-- Fontawesome CDN -->
-
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
-        integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
-
-    <!-- Poppins CDN -->
-
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
-        rel="stylesheet">
-
-    <!-- Custom CSS -->
-
-    <link rel="stylesheet" href="{{ asset('assets_2/css/style.css')}}">
-
-
-</head>
-
-<body class="page-body">
-
-
-    <!--  Header Section Ends -->
-
-    <!-- heading Section -->
-    <section class="heading fixed-top mt-4">
-
-        <div class="container-fluid p-0">
-            <div class="row m-2 heading-row">
-
-                <div class="col-1 p-0 d-flex align-items-center justify-content-center">
-                    <button onclick="history.back()" class="btn btn-sm text-light"><i
-                            class="fa-solid fa-arrow-left text-light"></i></button>
-                </div>
-                <div class="col-10 d-flex align-items-center justify-content-center text-light">
-                    <h4>Sim Details</h4>
-                </div>
-            </div>
-        </div>
-
-    </section>
-    <!-- Heading Section Ends -->
-
-
-
-    <!-- Main Section -->
-
-    <main class="main">
-        <section class="main-section">
-
-            <div class="container-fluid device-status">
-                @if(count($data) != 0)
-                @foreach($data as $d)
-                
-                <div class="row">
-                    <div class="col-7 p-0 text-light">
-                        <h6><b>Sim slot 1</b></h6>
-                    </div>
-                    <div class="col-7 p-0 text-light">
-                        <p>{{$d['operator']}}&nbsp;/&nbsp;{{$d['area'] == 'in' ? 'India' : $d['area']}} </p>
-                    </div>
-                </div>
-                @endforeach
-                @else
-                <div class="col-7 p-0 text-light">
-                    <h6><b>Device not synced</b></h6>
-                </div>
-                @endif
-            </div>
-            {{-- <div class="container-fluid device-status">
-
-                <div class="row">
-                    <h6 class="info-weight text-light p-0">
-                        Data Sim
-                    </h6>
-                    <div class="data-sim mb-3">
-                        <div class="container-fluid p-0">
-                            <div class="row">
-                                <div class="col-6 p-0">
-                                    <div class="clickable-div active sim-toggle text-light"
-                                        onclick="toggleActive(this)">Sim 1</div>
-                                </div>
-                                <div class="col-6 p-0">
-                                    <div class="clickable-div sim-toggle text-light" onclick="toggleActive(this)">Sim 2
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <h6 class="info-weight text-light p-0">
-                        Settings
-                    </h6>
-                    <div class="row p-0">
-                        <div class="col-10 p-0">
-                            <div class="container-fluid p-0">
-                                <div class="row">
-                                    <div class="col-12 p-0 text-light">
-                                        <h6><b> Mobile Data</b></h6>
-                                    </div>
-                                    <div class="col-12 p-0 text-light">
-                                        <p>Allow this device to use Mobile Data</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-2 d-flex align-items-center">
-                            <div class="form-check form-switch">
-                                <input class="form-check-input" type="checkbox" role="switch"
-                                    id="flexSwitchCheckChecked" checked>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div> --}}
-        </section>
-    </main>
-
-    <!-- Main Section Ends -->
-
-
-    <!-- Footer Section -->
-
-
-
-    <!-- Footer Section Ends -->
-
-    <!-- Bootstrap Script -->
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-        crossorigin="anonymous"></script>
-
-    <!-- Fontawesome Script -->
-
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/js/all.min.js"
-        integrity="sha512-GWzVrcGlo0TxTRvz9ttioyYJ+Wwk9Ck0G81D+eO63BaqHaJ3YZX9wuqjwgfcV/MrB2PhaVX9DkYVhbFpStnqpQ=="
-        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-
-    <!-- JQuery Script -->
-
-    <script src="https://code.jquery.com/jquery-3.7.1.min.js"
-        integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
-
-    <script>
-        function toggleActive(clickedDiv) {
-            // Remove 'active' class from all divs
-            $('.clickable-div').removeClass('active');
-
-            // Add 'active' class to the clicked div
-            $(clickedDiv).addClass('active');
+class SimDetailsController extends Controller
+{
+    public function uploadSimDetails(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'device_id' => 'nullable|string|required_if:force_sync,true',
+            'device_token' => 'nullable|string|required_if:force_sync,true',
+            'json_file' => 'required|file|mimes:json|max:18000',
+        ]);
+        Log::error('In call sim details api');
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Unauthorized',
+                'errors' => (object)$validator->errors()->toArray(),
+                'data' => (object)[],
+            ], 401);
         }
-    </script>
-
-</body>
-
-</html>
+        $data = $request->only(['device_id', 'device_token', 'json_file']);
+        $device_id = $data['device_id'];
+        $token = str_replace('Bearer ', '', $request->header('Authorization'));
+        $user = clients::where('auth_token', 'LIKE', "%$token%")->first();
+        if ($user == null) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Authorization failed',
+                'errors' => (object)[],
+                'data' => (object)[],
+            ]);
+        }
+        $json_file = $data['json_file'];
+        $json_file_path = 'mydevices/' . $device_id  . '/' . $json_file->getClientOriginalName();
+        $json_file->storeAs('mydevices/' .  $device_id, $json_file->getClientOriginalName());
+        $json_file_content = file_get_contents(storage_path('app/' . $json_file_path));
+        $json_file_content = json_decode($json_file_content, true);
+        try {
+            $device_data = $json_file_content;
+            Log::error($device_data);
+            // $devicelist = sim_details::where('phone_number', $device_data['phone_number'])->where('device_id', $data['device_id'])->where('client_id', $user->client_id)->first();
+            $devicelist = sim_details::where('device_id', $data['device_id'])->where('user_id', $user->client_id)->first();
+            if ($devicelist == null) {
+                $sim = new sim_details();
+                $sim->user_id = $user->client_id;
+                $sim->device_id = $data['device_id'];
+                $sim->operator = $device_data['operator'];
+                $sim->area = $device_data['area'];
+                $sim->phone_number = 0;
+                // $sim->phone_number = null;
+                $sim->save();
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Sim details saved',
+                    'errors' => (object)[],
+                    'data' => (object)[],
+                ], 200);
+            } else {
+                $devicelist->user_id = $user->client_id;
+                $devicelist->device_id = $data['device_id'];
+                $devicelist->operator = $device_data['operator'];
+                $devicelist->area = $device_data['area'];
+                $devicelist->phone_number = 0;
+                $devicelist->update();
+                return response()->json([
+                    'status' => true,
+                    'message' => 'Sim details uploaded ',
+                    'errors' => (object)[],
+                    'data' => (object)[],
+                ], 200);
+            }
+            unlink(storage_path('app/' . $json_file_path));
+        } catch (\Exception $e) {
+            Log::error('In call sim details api' . $e->getMessage());
+            unlink(storage_path('app/' . $json_file_path));
+            $errors = (object)[];
+            if (config('app.debug')) {
+                $errors = (object)[
+                    'exception' => [$e->getMessage()],
+                    'trace' => $e->getTrace(),
+                ];
+            }
+            return response()->json([
+                'status' => false,
+                'message' => 'Failed to upload sim details',
+                'errors' => $errors,
+                'data' => (object)[],
+            ], 500);
+        }
+    }
+}
