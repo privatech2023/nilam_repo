d51451b3f329835da42a7ee16c3f54940b4cba80
privatech2023 <154963732+privatech2023@users.noreply.github.com>
2024-05-03 19:28:54 +0530

    Update packages.blade.php

--- resources/views/frontend/pages/subscription/packages.blade.php
+++ resources/views/frontend/pages/subscription/packages.blade.php
@@ -169,7 +169,7 @@
 </style>
 {{-- activation modal --}}
 <div class="modal fade" id="activation-modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-    <div class="modal-dialog modal-dialog modal-sm" role="document">
+    <div class="modal-dialog modal-dialog-centered" role="document">
       <div class="modal-content">
         <div class="modal-header">
           <h5 class="modal-title" id="exampleModalLongTitle">Activation Code</h5>
