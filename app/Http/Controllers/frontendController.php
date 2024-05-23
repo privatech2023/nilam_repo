958f1315aa68c2123a85ff5f41e61079bbae6976
privatech2023 <154963732+privatech2023@users.noreply.github.com>
2024-05-23 20:58:37 +0530

    Update frontendController.php

--- app/Http/Controllers/frontendController.php
+++ app/Http/Controllers/frontendController.php
@@ -86,6 +86,7 @@ class frontendController extends Controller
         $response = curl_exec($ch);
         curl_close($ch);
         $response = json_decode($response, true);
+        dd($response);
         if ($response['status'] == 'success') {
             return true;
         } else {
